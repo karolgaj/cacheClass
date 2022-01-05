15abc7eb1a8bd8acbe9ed1b99f357ad8748724f9
Karol Gaj <karol.gaj93@gmail.com>
2022-01-05 13:43:27 +0100

    cache decorator

--- src/app/data.component.ts
+++ src/app/data.component.ts
@@ -11,17 +11,20 @@ import {DataService} from "./data.service";
       <p>Gender: {{data.gender}}</p>
     </div>
 
-    <button (click)="reloadData()">reload data</button>
+    <div *ngIf="data1$ | async as data">
+      <h1>User: {{ data.name.title }} {{ data.name.first }} {{ data.name.last }}</h1>
+      <h2>Username: {{data.login.username}}</h2>
+      <p>Phone: {{data.phone}}</p>
+      <p>Gender: {{data.gender}}</p>
+    </div>
+
     <button [routerLink]="'../placeholder'">go to PLACEHOLDER</button>
   `
 })
 
 export class DataComponent {
   public data$ = this.dataService.randomUser();
+  public data1$ = this.dataService.randomUser1();
   constructor(private dataService: DataService) {}
 
-
-  public reloadData(): void {
-    this.dataService.randomUserReload()
-  }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,45 +4,39 @@ import {merge, Observable, Subject} from "rxjs";
 import {map, mergeMap, shareReplay, take, takeUntil} from "rxjs/operators";
 import {Response, User} from "./data.model";
 
-declare global {
-  interface CacheableObservable<T> extends Observable<T> {
-    reload: () => void;
-  }
-}
-
 interface CacheableParameters { reloadable: boolean, bufferSize?: number }
-function Cacheable({ reloadable = false, bufferSize = 1 }: CacheableParameters ) {
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+function Cacheable({ reloadable = false, bufferSize = 1 }: CacheableParameters = { reloadable:false, bufferSize: 1 }) {
+  return function (target: any, propertyKey: string, descriptor: any) {
     const originalFunc = descriptor.value;
 
-    Object.defineProperty(target, 'reload$', {
+    Object.defineProperty(descriptor, 'reload$', {
       value: new Subject<void>()
     });
 
-    Object.defineProperty(target, 'getDataOneTime', {
-      value: () => target.data().pipe(take(1))
+    Object.defineProperty(descriptor, 'getDataOneTime', {
+      value: () => descriptor.data().pipe(take(1))
     });
 
-    Object.defineProperty(target, 'reloads$',{
-      value: target.reload$.pipe(mergeMap(() => target.getDataOneTime()))
+    Object.defineProperty(descriptor, 'reloads$',{
+      value: descriptor.reload$.pipe(mergeMap(() => descriptor.getDataOneTime()))
     })
 
     descriptor.value = function(this, args: any) {
-      target.data = () => {
-        if (!target.cache$) {
+      descriptor.data = () => {
+        if (!descriptor.cache$) {
           const result = originalFunc.apply(this, Array.isArray(args) ? args : [args]);
-          target.cache$ = result.pipe(takeUntil(target.reload$), shareReplay(bufferSize));
+          descriptor.cache$ = result.pipe(takeUntil(descriptor.reload$), shareReplay(bufferSize));
         }
-        return target.cache$;
+        return descriptor.cache$;
       };
 
-      target.data$ = merge(target.getDataOneTime(), target.reloads$);
+      descriptor.data$ = merge(descriptor.getDataOneTime(), descriptor.reloads$);
 
       if (reloadable) {
         Object.defineProperty(this, `${propertyKey}Reload`,{
           value: () => {
-            target.cache$ = null;
-            target.reload$.next();
+            descriptor.cache$ = null;
+            descriptor.reload$.next();
           },
           writable: true
         })
@@ -50,12 +44,12 @@ function Cacheable({ reloadable = false, bufferSize = 1 }: CacheableParameters )
 
       Object.defineProperty(this, `${propertyKey}Clear`,{
         value: () => {
-          target.cache$ = null;
+          descriptor.cache$ = null;
         },
         writable: true
       })
 
-      return target.data$
+      return descriptor.data$
     }
   };
 }
@@ -63,13 +57,16 @@ function Cacheable({ reloadable = false, bufferSize = 1 }: CacheableParameters )
 @Injectable({ providedIn: 'root' })
 export class DataService {
   private readonly apiUrl = 'https://randomuser.me/api/';
-  public randomUserReload!: () => void;
-  public randomUserClear!: () => void;
 
   constructor(private httpClient: HttpClient) {}
 
-  @Cacheable({ reloadable: true })
+  @Cacheable()
   public randomUser(): Observable<User> {
+    return this.httpClient.get<Response>(this.apiUrl).pipe(map((data: Response) => data?.results[0]));
+  }
+
+  @Cacheable({ reloadable: true })
+  public randomUser1(): Observable<User> {
     return this.httpClient.get<Response>(this.apiUrl).pipe(map((data: Response) => data?.results[0]));
   }
 }
